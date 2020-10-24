6752e5d3fa3110112cd1fd2f1fde93f1efd5f64e
AigerimBazarbayeva <ai.bazarbayeva@gmail.com>
2020-10-24 14:03:38 +0200

    Finished vue boilerplate setup and created basic table component.

---
+++ frontend/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Solar System Planets</h1>
+
+    <planet-table :planets="planets" />
+  </div>
+</template>
+
+<script>
+
+import PlanetTable from '@/components/PlanetTable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PlanetTable,
+  },
+  data() {
+    return {
+      planets: [
+        {
+          id: 1,
+          name: 'Earth',
+          radius: '100',
+          distance: '100',
+        },
+        {
+          id: 2,
+          name: 'Earth',
+          radius: '100',
+          distance: '100',
+        },
+        {
+          id: 3,
+          name: 'Earth',
+          radius: '100',
+          distance: '100',
+        },
+      ],
+    }
+  },
+}
+
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
