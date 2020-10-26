dd5e33323803d0348f32d52fd2240edbdeae3b55
AigerimBazarbayeva <ai.bazarbayeva@gmail.com>
2020-10-26 22:06:56 +0100

    Modified server file and successfully connected to MongoDb, created Planet model

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,24 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const { PORT, mongoUri } = require('./config')
+const cors = require('cors')
+const morgan = require('morgan')
+const planetRoutes = require('./routes/api/planets')
 
-// function returns a valid port, whether it is provided as a number or a string
-const normalizePort = val => {
-    const port = parseInt(val, 10);
+app.use(cors())
+app.use(morgan('tiny'))
 
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+mongoose
+    .connect(mongoUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+            useFindAndModify: false
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
 
-//checks for various errors and handles them appropriately — it is then registered to the server
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
+app.use('/api/planets', planetRoutes)
 
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-//logging the port or named pipe on which the server is running to the console
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
-});
-
-server.listen(port);
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
